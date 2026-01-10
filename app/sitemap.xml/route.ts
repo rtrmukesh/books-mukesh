47568a04e2868550395832db46a815f6d8f03e72
Mukesh-Thidiff <mukesh.murugaiyan@gmail.com>
2026-01-10 10:54:22 +0530

    wvwevwevwev

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const baseUrl = "https://books.themukesh.com";
+  const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
+    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <sitemap><loc>${baseUrl}/sitemap/0.xml</loc></sitemap>
+      <sitemap><loc>${baseUrl}/sitemap/1.xml</loc></sitemap>
+    </sitemapindex>`;
+
+  return new NextResponse(sitemapIndex, {
+    headers: { 'Content-Type': 'application/xml' },
+  });
+}
